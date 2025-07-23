10ed90723d85d98fc5cd2994cda3d44024a666f0
Swayam Jethi <swayam.jethi@gmail.com>
2025-07-23 13:02:44 +0530

    Added toast notification feature

--- AppUi/src/main.jsx
+++ AppUi/src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import Store from './store/store.jsx'
+import { ToastProvider } from './store/toastSlice.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </Provider>
 )
--- AppUi/src/pages/landing.jsx
+++ AppUi/src/pages/landing.jsx
@@ -1,10 +1,13 @@
 import { Navigate, useNavigate } from "react-router-dom";
 import { GUEST_PATH } from "../routes/guest/GuestPaths";
 
+import {useToast} from "../store/toastSlice.jsx";
 const LandingPage = () => {
     const navigate = useNavigate();
+    const { showToast } = useToast();
     const btnHandle = () => {
-        navigate(GUEST_PATH.LOGIN);
+        //navigate(GUEST_PATH.LOGIN);
+        showToast({ message: "Login Successful!", type: "success" });
     }
     return <>
         <div>
