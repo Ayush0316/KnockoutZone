3b7b92b2818ea61597581e4dd7b2737ebc03ae43
Swayam Jethi <swayam.jethi@gmail.com>
2025-07-24 17:25:47 +0530

    Add toast notification system using React Context

--- AppUi/src/pages/landing.jsx
+++ AppUi/src/pages/landing.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 import Button from "../components/common/Buttons/ButtonComponent";
-
- feature/toast-notification
-import {useToast} from "../store/toastSlice.jsx";
-const LandingPage = () => {
-    const navigate = useNavigate();
-    const { showToast } = useToast();
-    const btnHandle = () => {
-        //navigate(GUEST_PATH.LOGIN);
-        showToast({ message: "Login Successful!", type: "success" });
-    }
-    return <>
-        <div>
-            Landing page
-        </div>
-            <button onClick={btnHandle}>login</button>
-    </>
+import { useDispatch } from "react-redux";
+import { showToast } from "../store/slices/toastSlice";
 
 export default function Landing() {
-  return <div>
-    Landing Page.
-    <Button variant='secondary'>Login</Button>
-    </div>;
- main
+  const dispatch = useDispatch();
+  const btnHandle = () => {
+    dispatch(showToast({ 
+        message: "Login Successful!", 
+        type: "" }));
+  };
+  return (
+    <div>
+      Landing Page. <br/>
+      <Button variant="secondary" onClick={btnHandle}>
+        Login
+      </Button>
+    </div>
+  );
 }
--- AppUi/src/store/toastSlice.jsx
+++ AppUi/src/store/toastSlice.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useCallback } from "react";
-import Toast from "../components/common/Toast";
+import Toast from "../components/common/Toasts/Toast";
 
 const ToastContext = createContext();
 
