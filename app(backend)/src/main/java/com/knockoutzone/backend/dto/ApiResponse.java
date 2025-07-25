bb37a1d2d036836d537d644c624f94307efbe44e
RasagnaRayasam <rasagna2154@gmail.com>
2025-07-25 19:21:38 +0530

    Added ApiResponse DTO with Lombok annotations and default values (#26)

--- app(backend)/src/main/java/com/knockoutzone/backend/dto/ApiResponse.java
+++ app(backend)/src/main/java/com/knockoutzone/backend/dto/ApiResponse.java
@@ -1,49 +1,17 @@
 package com.knockoutzone.backend.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class ApiResponse<T> {
-    private boolean success;
-    private int statusCode;
-    private String message;
-    private T data;
 
-    public ApiResponse() {}
-
-    public ApiResponse(boolean success, int statusCode, String message, T data) {
-        this.success = success;
-        this.statusCode = statusCode;
-        this.message = message;
-        this.data = data;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
+    private boolean success = true;
+    private int statusCode = 200;
+    private String message = null;
+    private T data = null;
 }
+
